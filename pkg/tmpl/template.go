f798eccb3fba835f883d4923e506fe60fe4c227a
Sergei Lukianov <me@slukjanov.name>
2025-08-20 16:05:01 -0700

    chore: trim spaces from tmpl before render
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/tmpl/template.go
+++ pkg/tmpl/template.go
@@ -6,12 +6,15 @@ package tmpl
 import (
 	"bytes"
 	"fmt"
+	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
 )
 
 func Render(name, tmplText string, data any) ([]byte, error) {
+	tmplText = strings.TrimSpace(tmplText)
+
 	tmpl, err := template.New(name).Funcs(sprig.FuncMap()).Option("missingkey=error").Parse(tmplText)
 	if err != nil {
 		return nil, fmt.Errorf("parsing template: %w", err)
